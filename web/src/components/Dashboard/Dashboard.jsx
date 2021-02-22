7ce46a513095b7a3e3358f3cdde28e096ed1ebce
Felipe França <felipe@multiway.com.br>
2021-02-22 09:56:31 -0300

    Test requests

--- web/src/components/Dashboard/Dashboard.jsx
+++ web/src/components/Dashboard/Dashboard.jsx
@@ -1,10 +1,35 @@
+import axios from 'axios';
 import React from 'react';
 import Breadcrumb from '../Header/Breadcrumbs';
+
+const Type = () => {
+
+    axios.post('http://localhost:52773/report/category/all', {
+        auth: { 
+           username: '_SYSTEM', 
+           password: 'SYS' 
+        }
+    })
+    .then((result) => {
+        console.log(result);
+    })
+    .catch((error) => {
+        console.log(error);
+    })
+
+    return (
+        <React.Fragment>
+
+        </React.Fragment>
+    )
+}
 
 const Dashboard = () => {
     return (
         <React.Fragment>
             <Breadcrumb/>
+
+            <Type/>
         </React.Fragment>
     );
 }
